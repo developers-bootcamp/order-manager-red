d926444b81013cd6c05573f3a9863d6889f106bc
123yael <119128695+123yael@users.noreply.github.com>
2023-06-19 11:21:18 +0300

    first commit

---
+++ src/main/java/com/sapred/order/manager/red/Application.java
@@ -0,0 +1,13 @@
+package com.sapred.order.manager.red;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sapred/order/manager/red/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapred.order.manager.red;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
