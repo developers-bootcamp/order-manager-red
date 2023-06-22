f1dddbdc6f7964abc631c1920002f51a639a9af5
Shirel Asayag <asayagshirel@gmail.com>
2023-06-22 09:48:44 +0300

    Create model with lombok based on technical design

--- src/main/java/com/sapred/ordermanagerred/model/OrderItems.java
+++ src/main/java/com/sapred/ordermanagerred/model/OrderItems.java
@@ -3,12 +3,10 @@ package com.sapred.ordermanagerred.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.annotation.Collation;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@Collation("OrderItems")
 public class OrderItems {
     private Product productId;
     private double amount;
