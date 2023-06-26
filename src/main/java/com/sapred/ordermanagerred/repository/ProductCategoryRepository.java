055795a4a5d928f5c01e397a8e7f43be467e1d7f
rochiblau <racelblau@gmail.com>
2023-06-26 13:29:27 +0300

    create ProductCategory without Permissions

---
+++ src/main/java/com/sapred/ordermanagerred/repository/ProductCategoryRepository.java
@@ -0,0 +1,12 @@
+package com.sapred.ordermanagerred.repository;
+
+import com.sapred.ordermanagerred.model.ProductCategory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProductCategoryRepository extends MongoRepository<ProductCategory, String> {
+
+    boolean existsByName(String name);
+}
