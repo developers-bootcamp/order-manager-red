bf4906e2bed5d5bc5eb9082cce1c37e45b9c1cf7
123yael <119128695+123yael@users.noreply.github.com>
2023-07-02 15:35:03 +0300

    change delete edit and getNames

--- src/main/java/com/sapred/ordermanagerred/controller/UserController.java
+++ src/main/java/com/sapred/ordermanagerred/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.sapred.ordermanagerred.controller;
 
 import com.sapred.ordermanagerred.dto.UserDTO;
+import com.sapred.ordermanagerred.exception.NoPermissionException;
+import com.sapred.ordermanagerred.model.User;
 import com.sapred.ordermanagerred.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/User")
@@ -21,24 +26,41 @@ public class UserController {
         return userService.logIn(email, password);
     }
 
-    // זה פונקציה שמכניסה נתונים רק בשביל לבדוק שההתחברות עובדת
     @GetMapping("/fill")
     public void fill() {
         userService.fill();
     }
 
-    @DeleteMapping("/deleteCustomer/{customerId}")
-    public ResponseEntity<Boolean> deleteCustomer(@RequestHeader("token") String token, @PathVariable("customerId") String customerId) {
-        return userService.deleteCustomer(token, customerId);
+    @DeleteMapping("/deleteUser/{userId}")
+    public ResponseEntity<Boolean> deleteUser(@RequestHeader("token") String token, @PathVariable("userId") String userId) {
+        try {
+            userService.deleteUser(token, userId);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (NoPermissionException e) {
+            return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @PutMapping("/editCustomer")
-    public ResponseEntity<Boolean> editCustomer(@RequestHeader("token") String token, @RequestBody UserDTO customer) {
-        return userService.editCustomer(token, customer);
+    @PutMapping("/editUser")
+    public ResponseEntity<Boolean> editUser(@RequestHeader("token") String token, @RequestBody User user) {
+        try {
+            userService.editUser(token, user);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (NoPermissionException e) {
+            return new ResponseEntity<>(false, HttpStatus.UNAUTHORIZED);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+        }
     }
 
-    @GetMapping("/getAllNames")
-    public List<String> getAllNames(@RequestHeader("token") String token) {
-        return userService.getAllNames(token);
+    @GetMapping("/getAllNamesOfCustomers")
+    public List<Map.Entry<String, String>> getAllNamesOfCustomers(@RequestHeader("token") String token) {
+        return userService.getAllNamesOfCustomers(token);
     }
 }
--- src/main/java/com/sapred/ordermanagerred/service/UserService.java
+++ src/main/java/com/sapred/ordermanagerred/service/UserService.java
@@ -1,19 +1,20 @@
 package com.sapred.ordermanagerred.service;
 
 import com.sapred.ordermanagerred.dto.UserDTO;
+import com.sapred.ordermanagerred.exception.NoPermissionException;
 import com.sapred.ordermanagerred.model.*;
 import com.sapred.ordermanagerred.repository.CompanyRepository;
 import com.sapred.ordermanagerred.repository.RoleRepository;
 import com.sapred.ordermanagerred.repository.UserRepository;
 import com.sapred.ordermanagerred.security.JwtToken;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class UserService {
@@ -30,7 +31,6 @@ public class UserService {
     @Autowired
     private CompanyRepository companyRepository;
 
-    // שימי לב: זו סתם פונקציה שמכניסה נתונים בשביל הבדיקה
     public void fill() {
         AuditData d = new AuditData(new Date(), new Date());
         Role roles = new Role("1", RoleOptions.CUSTOMER, "cust", d);
@@ -53,53 +53,38 @@ public class UserService {
     }
 
 
-    public ResponseEntity<Boolean> deleteCustomer(String token, String customerId) {
+    @SneakyThrows
+    public void deleteUser(String token, String userId) {
         RoleOptions role = jwtToken.getRoleIdFromToken(token);
         String companyIdFromToken = jwtToken.getCompanyIdFromToken(token);
-        User user = userRepository.findById(customerId).get();
-        // אם הוא לא מנהל או שהוא לא מהחברה של הלקוח שאותו הוא רוצה למחוק
+        User user = userRepository.findById(userId).get();
         if (role == RoleOptions.CUSTOMER || !user.getCompanyId().getId().equals(companyIdFromToken))
-            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
-        try {
-            userRepository.deleteById(customerId);
-            return new ResponseEntity<>(true, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-        } catch (Exception e) {
-            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
-        }
+            throw new NoPermissionException("You do not have the appropriate permission to delete user");
+        userRepository.deleteById(userId);
     }
 
-    public ResponseEntity<Boolean> editCustomer(String token, UserDTO customer) {
+    @SneakyThrows
+    public void editUser(String token, User user) {
         RoleOptions role = jwtToken.getRoleIdFromToken(token);
         String companyIdFromToken = jwtToken.getCompanyIdFromToken(token);
-        User user = userRepository.findById(customer.getId()).get();
-        // אם הוא לא מנהל או שהוא לא מהחברה של הלקוח שאותו הוא רוצה לערוך
+        User userTOEdit = userRepository.findById(user.getId()).get();
         if (role == RoleOptions.CUSTOMER || !user.getCompanyId().getId().equals(companyIdFromToken))
-            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
-        try {
-            user.setFullName(customer.getFullName());
-            Address address = new Address(customer.getPhone(), customer.getAddress(), customer.getEmail());
-            user.setAddress(address);
-            user.getAuditData().setUpdateDate(new Date());
-            userRepository.save(user);
-            return new ResponseEntity<>(true, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
-        } catch (Exception e) {
-            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
-        }
+            throw new NoPermissionException("You do not have the appropriate permission to edit user");
+        userTOEdit.setFullName(user.getFullName());
+        Address address = new Address(user.getAddress().getPhone(),
+                user.getAddress().getName(), user.getAddress().getEmail());
+        userTOEdit.setAddress(address);
+        userTOEdit.getAuditData().setUpdateDate(new Date());
+        userRepository.save(user);
     }
 
-    public List<String> getAllNames(String token) {
+    public List<Map.Entry<String, String>> getAllNamesOfCustomers(String token) {
         String companyIdFromToken = jwtToken.getCompanyIdFromToken(token);
         List<User> us = userRepository.getAllByCompanyId(companyIdFromToken)
-                .stream()
-                .filter(u -> u.getRoleId().getName().equals(RoleOptions.CUSTOMER))
-                .toList();
-        List<String> listNames = new ArrayList<>();
+                .stream().filter(u -> u.getRoleId().getName().equals(RoleOptions.CUSTOMER)).toList();
+        List<Map.Entry<String, String>> listNames = new ArrayList<>();
         for (User user : us)
-            listNames.add(user.getFullName());
+            listNames.add(new HashMap.SimpleEntry<>(user.getId(), user.getFullName()));
         return listNames;
     }
 
