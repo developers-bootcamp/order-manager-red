672d64e338da5fb582a9437eb7abbb8ff39a2681
rochiblau <racelblau@gmail.com>
2023-07-02 17:09:28 +0300

    signUp api, not perfect

--- src/main/java/com/sapred/ordermanagerred/model/Company.java
+++ src/main/java/com/sapred/ordermanagerred/model/Company.java
@@ -2,11 +2,8 @@ package com.sapred.ordermanagerred.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Generated;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.annotation.Collation;
-import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -15,7 +12,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "Company")
 public class Company {
     @Id
-    @Generated
     private String id;
     private String name;
     private int currency;
