fb9aed39eb1f2ab3a0459653570bd12cff480bb2
Shirel Asayag <asayagshirel@gmail.com>
2023-07-02 09:29:01 +0300

    change model and product api

---
+++ src/main/java/com/sapred/ordermanagerred/dto/productMapper.java
@@ -0,0 +1,24 @@
+package com.sapred.ordermanagerred.dto;
+
+import com.sapred.ordermanagerred.model.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+
+@Mapper(componentModel = "spring")
+public interface productMapper {
+    productMapper INSTANCE = Mappers.getMapper(productMapper.class);
+
+    @Mapping(source = "productCategoryId.id",target = "productCategoryId")
+    ProductDTO productToDto(Product p);
+
+    @Mapping(source = "productCategoryId",target = "productCategoryId.id")
+    Product dtoToProduct(ProductDTO p);
+
+    List<ProductDTO> productToDto(List<Product> list);
+    List<Product> dtoToProduct(List<ProductDTO> list);
+
+}
