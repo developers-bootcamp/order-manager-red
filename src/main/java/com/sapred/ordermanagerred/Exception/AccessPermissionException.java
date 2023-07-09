4ad2fd6bee43f08fa50096ef40ab6e8f5a6cc3e8
rochiblau <racelblau@gmail.com>
2023-07-09 09:58:03 +0300

    create productCategory

---
+++ src/main/java/com/sapred/ordermanagerred/Exception/AccessPermissionException.java
@@ -0,0 +1,7 @@
+package com.sapred.ordermanagerred.Exception;
+
+public class AccessPermissionException extends RuntimeException {
+    public AccessPermissionException(String massage) {
+        super(massage);
+    }
+}
