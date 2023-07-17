109ce6dce4fa7411730b1e96a240b8424f8cfe61
123yael <119128695+123yael@users.noreply.github.com>
2023-07-17 19:01:34 +0300

    change list of map result to UserNameDTO

---
+++ src/main/java/com/sapred/ordermanagerred/dto/UserNameDTO.java
@@ -0,0 +1,13 @@
+package com.sapred.ordermanagerred.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserNameDTO {
+    private String id;
+    private String name;
+}
