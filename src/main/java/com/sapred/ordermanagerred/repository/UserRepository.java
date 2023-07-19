9ab17542669158759881bbac530318a65d81ac0c
Ruti Churi <112867662+RutiChuri@users.noreply.github.com>
2023-07-19 11:56:52 +0300

    create and getAll Users_2 (#27)

--- src/main/java/com/sapred/ordermanagerred/repository/UserRepository.java
+++ src/main/java/com/sapred/ordermanagerred/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.sapred.ordermanagerred.repository;
 
 import com.sapred.ordermanagerred.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,11 +14,13 @@ import java.util.List;
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByAddressEmail(String email);
 
+    boolean existsByAddress_Email(String email);
     @Autowired
     User getByAddressEmail(String email);
 
     @Autowired
     @Query("{'companyId.id': ?0, 'roleId.id': ?1, 'fullName': { $regex: '^?2' }}}")
     List<User> findByCompanyIdAndRoleIdAndPrefix(String companyId, String roleId, String prefix);
+    Page<User> findByCompanyId(String companyId, Pageable pageable);
 
 }
