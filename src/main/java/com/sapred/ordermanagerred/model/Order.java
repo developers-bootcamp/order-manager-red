1b08f097fc807826a2b0a052180809113f5d7cf3
rochiblau <racelblau@gmail.com>
2023-07-23 11:39:16 +0300

    changes

--- src/main/java/com/sapred/ordermanagerred/model/Order.java
+++ src/main/java/com/sapred/ordermanagerred/model/Order.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Data
