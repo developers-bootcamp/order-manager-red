72a59f2654e527ab85d32fe65b4de5469d022f4a
ChaniDonat <136927748+ChaniDonat@users.noreply.github.com>
2023-07-23 17:03:28 +0300

    TopEmployee Aggrigation

--- src/main/java/com/sapred/ordermanagerred/controller/GraphController.java
+++ src/main/java/com/sapred/ordermanagerred/controller/GraphController.java
@@ -1,19 +1,27 @@
 package com.sapred.ordermanagerred.controller;
 
+import com.mongodb.client.AggregateIterable;
+import com.sapred.ordermanagerred.dto.TopEmployeeDTO;
 import com.sapred.ordermanagerred.service.GraphService;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/Graph")
+@CrossOrigin("http://localhost:3000")
 public class GraphController {
     @Autowired
     private GraphService graphService;
 
     @GetMapping("/topEmployee")
-    public void topEmployee() {
-        graphService.topEmployee();
+    public List<TopEmployeeDTO> topEmployee() {
+       return graphService.topEmployee();
     }
 }
--- src/main/java/com/sapred/ordermanagerred/service/GraphService.java
+++ src/main/java/com/sapred/ordermanagerred/service/GraphService.java
@@ -1,48 +1,64 @@
 package com.sapred.ordermanagerred.service;
 
+import com.mongodb.client.AggregateIterable;
 import com.sapred.ordermanagerred.dto.TopEmployeeDTO;
-import com.sapred.ordermanagerred.model.User;
 import com.sapred.ordermanagerred.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.aggregation.GroupOperation;
-import org.springframework.data.mongodb.core.aggregation.MatchOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.CriteriaDefinition;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.data.mongodb.core.aggregation.*;
+import org.bson.Document;
 
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.domain.Sort;
+import org.bson.Document;
 import java.time.LocalDate;
-import java.util.List;
+import java.util.*;
 
+import static com.mongodb.client.model.Projections.exclude;
+import static com.mongodb.client.model.Projections.slice;
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.project;
 
 @Service
 public class GraphService {
-    //    @Autowired
-//    private OrderRepository orderRepository;
+    @Autowired
+    private OrderRepository orderRepository;
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    public void topEmployee() {
+    //    private Document threeMonthsAgo() {
+//        return new Document("$date", new Document("$subtract", Arrays.asList(new Document("$date", new Document("$now", new Document())), new Document("$numberLong", 3 * 30 * 24 * 60 * 60))).toJson());
+//    }
+    public List<TopEmployeeDTO> topEmployee() {
         LocalDate threeMonthsAgo = LocalDate.now().minusMonths(3);
         Aggregation aggregation = newAggregation(
-                match(Criteria.where("orderStatusId").is("Done").and("auditData.createDate").gte(threeMonthsAgo)),
-                group("employee").count().as("CountOfDeliveredOrders"),
-                project("CountOfDeliveredOrders").and("employeeId").previousOperation(),
-                sort(Sort.Direction.DESC, "CountOfDeliveredOrders"),
+                match(
+                        new Criteria().where("orderStatus").is("DONE").and("auditData.createDate").gte(LocalDate.now().minusMonths(3))
+                ),
+                group("employeeId").count().as("CountOfDeliveredOrders"),
+               lookup("User", "_id.employeeId", "_id", "employee"),
+//                unwind("employee"),
+                sort(Sort.by(Sort.Direction.DESC, "CountOfDeliveredOrders")),
                 limit(5)
         );
-        AggregationResults<TopEmployeeDTO> result = mongoTemplate.aggregate(
-                aggregation, "Order", TopEmployeeDTO.class);
-//        List<TopEmployeeDTO> x = result.getMappedResults();
-//        for (TopEmployeeDTO employee : result.getMappedResults()) {
-//            System.out.println(employee.getUser().getFullName());
-//        }
-        List<TopEmployeeDTO> resultList = result.getMappedResults();
-        for (TopEmployeeDTO employee : resultList) {
-            System.out.println("Employee FullName: " + employee.getUser().getFullName());
-        }
+        AggregationResults<Document> groupResults = mongoTemplate.aggregate(aggregation, "Order", Document.class);
+        System.out.println(groupResults.getMappedResults());
 
+        List<TopEmployeeDTO> result = new ArrayList<>();
+        for (Document document : groupResults) {
+            TopEmployeeDTO topEmployeeDTO = new TopEmployeeDTO();
+            topEmployeeDTO.setCountOfDeliveredOrders(document.getInteger("CountOfDeliveredOrders"));
+            System.out.println("CountOfDeliveredOrders " + topEmployeeDTO.getCountOfDeliveredOrders());
+            result.add(topEmployeeDTO);
+        }
+        return  result;
+//
     }
 }
