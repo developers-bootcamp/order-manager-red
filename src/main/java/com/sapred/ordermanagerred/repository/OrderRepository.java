cb59e43295903e28692ca7a8966a6df7fc6534cc
ShulamitTol <136656002+ShulamitTol@users.noreply.github.com>
2023-07-24 12:53:58 +0300

    add filteing

--- src/main/java/com/sapred/ordermanagerred/repository/OrderRepository.java
+++ src/main/java/com/sapred/ordermanagerred/repository/OrderRepository.java
@@ -1,13 +1,38 @@
 package com.sapred.ordermanagerred.repository;
 
 import com.sapred.ordermanagerred.model.Order;
+import com.sapred.ordermanagerred.model.OrderItem;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends MongoRepository<Order, String> {
     Page<Order> findByCompanyId_IdAndOrderStatusAndEmployeeId(String companyId, String orderStatusId, String employee, Pageable pageable);
+
+
+        @Query("{"
+                + "?#{( #companyId == null || #companyId == '') ? {_id: {$exists: true}} : {'companyId.$id': #companyId}}, "
+                + "?#{( #orderStatusId == null || #orderStatusId == '') ? {_id: {$exists: true}} : {'orderStatus': #orderStatusId}}, "
+                + "?#{( #employee == null || #employee == '') ? {_id: {$exists: true}} : {'employeeId.$id': #employee}}, "
+                + "?#{( #totalAmount == null ) ? {_id: {$exists: true}} : {'totalAmount': #totalAmount}}, "
+                + "?#{( #orderItemsList == null ) ? {_id: {$exists: true}} : {'orderItemsList': #orderItemsList}}, "
+                + "?#{( #creditCardNumber == null ) ? {_id: {$exists: true}} : {'creditCardNumber': #creditCardNumber}}, "
+                + "?#{( #ExpireOn == null ) ? {_id: {$exists: true}} : {'ExpireOn': #ExpireOn}}, "
+                + "?#{( #cvc == null ) ? {_id: {$exists: true}} : {'cvc': #cvc}}, "
+                + "?#{( #notificationFlag == null ) ? {_id: {$exists: true}} : {'notificationFlag': #notificationFlag}}"
+                + "}")
+        Page<Order> findByParams(String companyId, String orderStatusId, String employee,
+                                 Optional<Integer> totalAmount, Optional<List<OrderItem>> orderItemsList,
+                                 Optional<Integer> creditCardNumber, Optional<Date> ExpireOn,
+                                 Optional<Integer> cvc, Optional<Boolean> notificationFlag,
+                                 Pageable pageable);
+
 
 }
