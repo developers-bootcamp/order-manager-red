2801a2e40dc2a29d1685b977e4ff34399c222c1b
rochiblau <racelblau@gmail.com>
2023-08-03 20:00:31 +0300

    aggregation

--- src/main/java/com/sapred/ordermanagerred/OrderManagerRedApplication.java
+++ src/main/java/com/sapred/ordermanagerred/OrderManagerRedApplication.java
@@ -1,5 +1,6 @@
 package com.sapred.ordermanagerred;
 
+import com.sapred.ordermanagerred.service.AggregationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
