ff914365c1d149db5c7087c6e9244afa8faec6f2
PninaFriedman <p0583289998@gmail.com>
2023-08-20 10:13:41 +0300

    change to graph files

--- src/main/java/com/sapred/ordermanagerred/controller/GraphController.java
+++ src/main/java/com/sapred/ordermanagerred/controller/GraphController.java
@@ -3,6 +3,8 @@ package com.sapred.ordermanagerred.controller;
 import com.sapred.ordermanagerred.dto.MonthProductCountDto;
 import com.sapred.ordermanagerred.service.GraphService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.sapred.ordermanagerred.dto.TopEmployeeDTO;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Month;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/graph")
@@ -35,4 +39,13 @@ public class GraphController {
         return graphService.getTopProduct(token, rangeOfMonths);
     }
 
+    @GetMapping("/status")
+    public ResponseEntity<Map<Month, Map<Integer,Integer>>> getStatus(@RequestHeader("token") String token) {
+        int monthAmount = 5;
+        try{
+            return ResponseEntity.ok(graphService.getStatus(token, monthAmount));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
--- src/main/java/com/sapred/ordermanagerred/service/GraphService.java
+++ src/main/java/com/sapred/ordermanagerred/service/GraphService.java
@@ -30,7 +30,10 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
 
@@ -104,5 +107,40 @@ public class GraphService {
         log.info("Top products retrieved: {}", topProduct);
 
         return topProduct;
+    }
+
+    public Map<Month, Map<Integer, Integer>> getStatus(String token, Integer monthAmount) {
+        String companyId = jwtToken.getCompanyIdFromToken(token);
+        LocalDate currentDate = LocalDate.now();
+        LocalDate MonthsAgo = currentDate.minusMonths(monthAmount);
+        Aggregation aggregation = newAggregation(
+                match(Criteria.where("auditData.createDate").gte(MonthsAgo)),
+                match(Criteria.where("companyId").is(companyId)),
+                project()
+                        .andExpression("month(auditData.createDate)").as("month")
+                        .and("orderStatusId").as("orderStatusId"),
+                group("month")
+                        .sum(ConditionalOperators.when(ComparisonOperators.valueOf("orderStatusId").equalToValue("0")).then(1).otherwise(0)).as("cancelled")
+                        .sum(ConditionalOperators.when(ComparisonOperators.valueOf("orderStatusId").equalToValue("1")).then(1).otherwise(0)).as("delivered"),
+                project()
+                        .and("_id").as("month")
+                        .and("cancelled").as("cancelled")
+                        .and("delivered").as("delivered")
+        );
+        AggregationResults<Document> results = mongoTemplate.aggregate(aggregation, "Order", Document.class);
+        List<Document> mappedResults = results.getMappedResults();
+        System.out.println("Results" + results);
+        System.out.println("mappedResults" + mappedResults);
+
+        Map<Month, Map<Integer, Integer>> resultMap = new HashMap<>();
+        for (Document mappedResult : mappedResults) {
+            Month month = Month.of(mappedResult.getInteger("month"));
+            int cancelled = mappedResult.getInteger("cancelled", 0);
+            int delivered = mappedResult.getInteger("delivered", 0);
+            Map<Integer, Integer> tempMap = new HashMap<>();
+            tempMap.put(cancelled, delivered);
+            resultMap.put(month, tempMap);
+        }
+        return resultMap;
     }
 }
