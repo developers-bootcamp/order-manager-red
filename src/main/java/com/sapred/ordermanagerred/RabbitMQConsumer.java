8a0fce9c564df68562c13e3a84c0271ee2452c1c
Shirel Asayag <asayagshirel@gmail.com>
2023-08-20 10:18:50 +0300

    do logic in consumer

--- src/main/java/com/sapred/ordermanagerred/RabbitMQConsumer.java
+++ src/main/java/com/sapred/ordermanagerred/RabbitMQConsumer.java
@@ -1,19 +1,46 @@
 package com.sapred.ordermanagerred;
 
 import com.sapred.ordermanagerred.dto.OrderDTO;
+import com.sapred.ordermanagerred.mapper.OrderMapper;
+import com.sapred.ordermanagerred.model.Order;
+import com.sapred.ordermanagerred.model.OrderItem;
+import com.sapred.ordermanagerred.model.OrderStatus;
+import com.sapred.ordermanagerred.model.Product;
+import com.sapred.ordermanagerred.repository.OrderRepository;
+import com.sapred.ordermanagerred.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQConsumer {
 
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private ProductRepository productRepository;
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
 
     @RabbitListener(queues = "${rabbitmq.queue.namePayment}")
     public void consume(OrderDTO message) {
         LOGGER.info(String.format("Received message -> %s ", message));
+        if (message.getOrderStatus()== OrderStatus.APPROVED){
+            message.setOrderStatus(OrderStatus.PACKING);
+            Order order= OrderMapper.INSTANCE.DTOToOrder(message);
+            orderRepository.save(order);
+        }
+        else {
+            message.setOrderStatus(OrderStatus.CANCELLED);
+            Order order= OrderMapper.INSTANCE.DTOToOrder(message);
+            orderRepository.save(order);
+            for (OrderItem element : order.getOrderItemsList()) {
+                Product product = (Product) productRepository.findOneByIdAndCompanyId(element.getProductId().getId(), order.getCompanyId().getId());
+                product.setInventory(product.getInventory()+element.getQuantity());
+                productRepository.save(product);
+            }
+        }
     }
 
 }
