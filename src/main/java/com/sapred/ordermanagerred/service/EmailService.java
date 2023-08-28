c71fb0110066b72e98742ec065534811a0a28b02
Shirel Asayag <asayagshirel@gmail.com>
2023-08-29 00:03:20 +0300

    send email without use

---
+++ src/main/java/com/sapred/ordermanagerred/service/EmailService.java
@@ -0,0 +1,36 @@
+package com.sapred.ordermanagerred.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}") private String sender;
+    public String sendSimpleMail(String fromStatus,String toStatus,String recipient ){
+        try {
+
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage
+                    = new SimpleMailMessage();
+
+            // Setting up necessary details
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(recipient);
+            mailMessage.setText("your order move from "+fromStatus+" to "+toStatus+"!!!");
+            mailMessage.setSubject("Status order");
+
+            // Sending the mail
+            javaMailSender.send(mailMessage);
+            return "Mail Sent Successfully...";
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
+    }
+
+}
