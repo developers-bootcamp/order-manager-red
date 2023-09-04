0594bcbfe33e9538f5a59c25359283d8f5bd79b9
Shirel Asayag <113021351+ShirelAs@users.noreply.github.com>
2023-09-04 13:00:17 +0300

    Send email (#64)
    * send email without use
    
    * finish email
    
    * Change from DEBUG to INFO
    
    * fix rabbitMQ

---
+++ src/main/java/com/sapred/ordermanagerred/service/EmailService.java
@@ -0,0 +1,38 @@
+package com.sapred.ordermanagerred.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}") private String sender;
+    public String sendSimpleMail(String toStatus,String recipient ){
+        try {
+
+            // Creating a simple mail message
+            SimpleMailMessage mailMessage
+                    = new SimpleMailMessage();
+
+            // Setting up necessary details
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(recipient);
+            mailMessage.setText("your order move to  "+toStatus+" status !!!");
+            mailMessage.setSubject("Status order");
+
+            // Sending the mail
+            javaMailSender.send(mailMessage);
+            return "Mail Sent Successfully...";
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+            return "Error while Sending Mail";
+        }
+    }
+
+}
